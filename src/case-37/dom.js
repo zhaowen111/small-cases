9f352cc615bbaf0cd7892d4f7498c7a75f30400e
zhaowen111 <2872186458@qq.com>
2021-04-16 16:39:41 +0800

    添加case-37 封装dom库

---
+++ src/case-37/dom.js
@@ -0,0 +1,101 @@
+window.dom = {
+  create(tagName) {
+    if (tagName.match(/<.*>/)) {
+      const container = document.createElement('template');
+      container.innerHTML = tagName.trim();
+      return container.content.firstChild;
+    } else {
+      return document.createElement(tagName);
+    }
+  },
+  insertAfter(node, target) {
+    target.parentNode.insertBefore(node, target.nextSibling);
+  },
+  insertBefore(node, target) {
+    target.parentNode.insertBefore(node, target);
+  },
+  wrap(node, parent) {
+    let oldParent = node.parentNode;
+    oldParent.appendChild(parent);
+    parent.appendChild(node);
+  },
+  remove(node) {
+    node.parentNode.removeChild(node);
+    return node;
+  },
+  empty(node) {
+    const arr = [];
+    while (node.firstChild) {
+      arr.push(this.remove(node.firstChild));
+    }
+    return arr;
+  },
+  attr(node, name, value) {
+    if (value !== undefined) {
+      return node.setAttribute(name, value);
+    }
+    return node.getAttribute(name);
+  },
+  text(node, str) {
+    if (!str) {
+      return node.innerText || node.textContent;
+    }
+    if ('innerText' in node) {
+      node.innerText = str;
+    } else if ('textContent' in node) {
+      node.textContent = str;
+    } else {
+      console.error('invalid node parameter');
+    }
+  },
+  style(node, options) {
+    if (options) {
+      for (let key in options) {
+        node.style[key] = options[key];
+      }
+    }
+    return node.style;
+  },
+  on(node, eventType, callback) {
+    return node.addEventListener(eventType, callback);
+  },
+  off(node, eventType, callback) {
+    return node.removeEventListener(eventType, callback);
+  },
+  find(selector, target) {
+    return (target || document).querySelectorAll(selector);
+  },
+  parent(node) {
+    return node.parentNode;
+  },
+  children(node) {
+    return Array.from(node.children);
+  },
+  siblings(node) {
+    return Array.from(node.parentNode.children).filter((n) => {
+      return n !== node;
+    });
+  },
+  next(node) {
+    return node.nextSibling;
+  },
+  prev(node) {
+    return node.previousSibling;
+  },
+  nextEle(node) {
+    let result = this.next(node);
+    if (result === null) return result;
+    return result.nodeType === 1 ? result : this.nextEle(result);
+  },
+  prevEle(node) {
+    let result = this.prev(node);
+    if (result === null) return result;
+    return result.nodeType === 1 ? result : this.prevEle(result);
+  },
+  indexOfParent(node) {
+    return this.children(node.parentNode).indexOf(node);
+  },
+  each(nodeList, callback) {
+    return Array.from(nodeList).forEach(callback);
+  }
+};
---
+++ src/case-37/index.js
@@ -0,0 +1,36 @@
+let div1 = dom.create('div');
+div1.style.width = '100px';
+div1.style.height = '100px';
+div1.style.background = 'red';
+let div2 = div1.cloneNode();
+document.body.appendChild(div1);
+
+let div3 = dom.create(
+  '<div style="background:black;width:200px;height:200px;"></div>'
+);
+dom.wrap(div1, div3);
+
+console.log(dom.attr(document.body, 'style'));
+console.log(dom.attr(document.body, 'style', 'background:black;height:500px'));
+console.log(dom.attr(document.body, 'style'));
+dom.style(document.body, { background: 'red', width: '5000px' });
+dom.on(document.body, 'click', (e) => {
+  alert(e.target);
+});
+
+let parent = dom.find('#siblings')[0];
+console.log(dom.children(parent));
+let firstChild = dom.children(parent)[0];
+console.log(dom.siblings(firstChild));
+let middleChild = dom.next(firstChild);
+console.log(middleChild);
+console.log(dom.prev(middleChild));
+console.log(dom.prev(firstChild));
+
+console.log('next/prev element');
+middleChild = dom.find('.div2')[0];
+console.log(middleChild);
+console.log(dom.nextEle(middleChild));
+console.log(dom.prevEle(middleChild));
+
+console.log(dom.indexOfParent(middleChild));
